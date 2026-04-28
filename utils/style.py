c8b424769f96f08a7d735d05e0bacd3ba21a49f3
wan123gunawan <133235654+wan123gunawan@users.noreply.github.com>
2026-04-28 12:12:01 +0700

    Create style.py

---
+++ utils/style.py
@@ -0,0 +1,7 @@
+def load_css():
+    return """
+    <style>
+    .stApp { background-color: #0e1117; color: white; }
+    .card { background:#161b22; padding:15px; border-radius:10px; }
+    </style>
+    """
